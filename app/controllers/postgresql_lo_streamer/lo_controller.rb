6e685366a7993aa3bf7a66138bf86d8fa0d90f7e
Diogo Biazus <diogob@gmail.com>
2012-09-28 11:56:21 -0300

    now we have a basic engine with one controller working, let's start working

--- app/controllers/postgresql_lo_streamer/lo_controller.rb
+++ app/controllers/postgresql_lo_streamer/lo_controller.rb
@@ -3,6 +3,7 @@
 module PostgresqlLoStreamer
   class LoController < ActionController::Base
     def stream
+      return render :nothing => true, :status => 200
     end
   end
 end
--- spec/controllers/postgresql_lo_streamer/lo_controller_spec.rb
+++ spec/controllers/postgresql_lo_streamer/lo_controller_spec.rb
@@ -4,7 +4,7 @@ describe PostgresqlLoStreamer::LoController do
   subject{ response }
   describe "GET stream" do
     before do
-      get :stream, :id => 1
+      get :stream, :id => 1, :use_route => :postgresql_lo_streamer
     end
     its(:status){ should == 200 }
   end
