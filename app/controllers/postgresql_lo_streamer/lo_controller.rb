b90f4635c069f9d66d943189aa4f5d60592364a6
edwardmp <e.poot@evolutionhost.nl>
2016-10-07 20:19:46 +0200

    Set status code to 404 if object does not exist

--- app/controllers/postgresql_lo_streamer/lo_controller.rb
+++ app/controllers/postgresql_lo_streamer/lo_controller.rb
@@ -4,16 +4,35 @@ module PostgresqlLoStreamer
   class LoController < ActionController::Base
     def stream
       send_file_headers! configuration.options
+
+      object_identifier = params[:id].to_i
+      if !object_exists?(object_identifier)
+        self.status = 404
+        render :nothing => true and return
+      end
+
       self.status = 200
       self.response_body = Enumerator.new do |y|
         connection.transaction do
-          lo = connection.lo_open(params[:id].to_i, ::PG::INV_READ)
+          lo = connection.lo_open(object_identifier, ::PG::INV_READ)
           while data = connection.lo_read(lo, 4096) do
             y << data
           end
           connection.lo_close(lo)
         end
       end
+    end
+
+    def object_exists?(identifier)
+      begin
+        connection.lo_open(identifier, ::PG::INV_READ)
+      rescue PG::Error => e
+        if e.to_s.include? "does not exist"
+          return false
+        end
+      end
+
+      return true
     end
 
     def connection
