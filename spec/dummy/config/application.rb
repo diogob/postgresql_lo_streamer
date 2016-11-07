579ae284c3940afa54d1804d832cbccda9929175
Edward Poot <e.poot@evolutionhost.nl>
2016-11-08 00:10:07 +0100

    Update application.rb

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -2,10 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
 require 'rails/all'
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
