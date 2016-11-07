1f8aaaa09917b8c3dfc527fec81a4f79b95dba34
Edward Poot <e.poot@evolutionhost.nl>
2016-11-08 00:04:20 +0100

    taking a stab at fixing the CI tests..

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
+require 'rails/all'
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
