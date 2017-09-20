0948cb1f4900e4e55f2ba0fcc58997e61209c126
Ryan Helsing <ryanhelsing@gmail.com>
2017-09-20 10:06:35 -0400

    a test

--- spec/controllers/postgresql_lo_streamer/lo_controller_spec.rb
+++ spec/controllers/postgresql_lo_streamer/lo_controller_spec.rb
@@ -33,6 +33,26 @@ describe PostgresqlLoStreamer::LoController do
     its(:status) { should == 200 }
   end
 
+  describe "GET mime stream" do
+    before do
+      expect(controller).to receive(:send_file_headers!).with({:type => "image/jpg", :disposition => "inline"})
+      connection.transaction do
+        @oid = connection.lo_creat
+        lo = connection.lo_open(@oid, ::PG::INV_WRITE)
+        size = connection.lo_write(lo, file_content)
+        connection.lo_close(lo)
+      end
+      get :stream, :id => @oid, :format => "jpg"
+    end
+
+    after do
+      connection.lo_unlink(@oid)
+    end
+
+    its(:body) { should == file_content }
+    its(:status) { should == 200 }
+  end
+
   describe "GET non-existing stream" do
     before do
       expect(controller).to receive(:send_file_headers!).with({:type => "image/png", :disposition => "inline"})
